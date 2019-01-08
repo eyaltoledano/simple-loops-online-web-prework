ff01338d6c2f8d9746f648787e2d1cbd1343e05d
Eyal Toledano <eyaltoledano@me.com>
2019-01-08 04:50:18 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -13,7 +13,7 @@ def loop_iterator(number_of_times)
 
   loop number_of_times.times do
     puts phrase
-    break if counter => number_of_times
+    break if counter > number_of_times
     counter += 1
   end
 end
