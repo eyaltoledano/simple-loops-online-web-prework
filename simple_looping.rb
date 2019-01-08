78314e1c65b5fb11b57c0ca1e0dc0b4911399851
Eyal Toledano <eyaltoledano@me.com>
2019-01-08 04:42:44 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -11,7 +11,7 @@ def loop_iterator(number_of_times)
   phrase = "Welcome to Flatiron School's Web Development Course!"
   counter = 1
 
-  loop (number_of_times)_to.n.times do
+  loop number_of_times.times do
     phrase
     break if counter == number_of_times
     counter += 1
